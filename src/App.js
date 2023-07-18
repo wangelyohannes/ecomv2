6800f212372f9c7db30eef65e57337be95497d7b
wangelyohannes <127981944+wangelyohannes@users.noreply.github.com>
2023-07-18 15:09:14 -0400

    almost...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import HeaderFooter from "./components/HeaderFooter";
 import Contact from "./pages/Contact";
